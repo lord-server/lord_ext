5edd9b6d26df3253e82d436d5af9acfee33cfbc1
weqqr <weqqr@opmbx.org>
2017-03-01 17:18:12 +0300

    mypos: Fix translation

--- mypos/init.lua
+++ mypos/init.lua
@@ -11,7 +11,7 @@ minetest.register_chatcommand("mypos", {
       local mypos = player:getpos()
       local message = string.format("x=%d, y=%d, z=%d", mypos.x,mypos.y,mypos.z)
       if param == "" then
-        minetest.chat_send_all("<"..name..">"..message)
+        minetest.chat_send_all("<"..name.."> "..message)
       elseif minetest.get_player_by_name(param) then
         minetest.chat_send_player(param, message)
       else
